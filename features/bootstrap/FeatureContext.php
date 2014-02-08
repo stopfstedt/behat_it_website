fe234b409321d09fdd8a8ee66ae8bcb2190d9682
Stefan Topfstedt <Stefan.Topfstedt@ucsf.edu>
2014-02-07 16:02:25 -0800

    started implementing feature context.

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,24 @@
+<?php
+
+use Behat\Behat\Context\ClosuredContextInterface,
+    Behat\Behat\Context\TranslatedContextInterface,
+    Behat\Behat\Context\BehatContext,
+    Behat\Behat\Exception\PendingException;
+use Behat\Gherkin\Node\PyStringNode,
+    Behat\Gherkin\Node\TableNode;
+
+use Behat\MinkExtension\Context\MinkContext;
+
+/**
+ * Features context.
+ */
+class FeatureContext extends MinkContext
+{
+    /**
+     * @Given /^I am on the home page$/
+     */
+    public function iAmOnTheHomePage ()
+    {
+        $this->visit("/");
+    }
+}
